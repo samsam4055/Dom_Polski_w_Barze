fd718c61c1387daf4db13d12abc7f591bcbea331
AndreyFrigoris <andreyfrigoris@gmail.com>
2016-10-20 20:57:49 +0300

    +

--- js/ideal-image-slider.js
+++ js/ideal-image-slider.js
@@ -403,7 +403,7 @@ var IdealImageSlider = (function() {
 			height: 'auto', // "auto" | px value (e.g. 400) | aspect ratio (e.g. "16:9")
 			initialHeight: 400, // for "auto" and aspect ratio
 			maxHeight: null, // for "auto" and aspect ratio
-			interval: 4000,
+			interval: 1000,
 			transitionDuration: 700,
 			effect: 'slide',
 			disableNav: false,
