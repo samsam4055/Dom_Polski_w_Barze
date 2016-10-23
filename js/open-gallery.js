b065de447eddc15414c19ec2066383e895722846
Komnatnaya <comina.ru@gmail.com>
2016-10-23 19:05:58 +0300

    Спрятана галлерея

---
+++ js/open-gallery.js
@@ -0,0 +1,9 @@
+var link = document.querySelector(".open-gallery");
+var gall = document.querySelector(".hidden-gallery");
+var btn = document.querySelector(".for-open");
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	gall.classList.add("hidden-gallery-show");
+    btn.classList.add("hidden-btn");
+});
