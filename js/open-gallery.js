0f9817e91f94cc8786ef5ee61e1eed2aecb0a187
Katerina <comina.ru@gmail.com>
2016-10-26 15:31:38 +0300

    ++

--- js/open-gallery.js
+++ js/open-gallery.js
@@ -1,9 +1,9 @@
-var link = document.querySelector(".open-gallery");
-var gall = document.querySelector(".hidden-gallery");
-var btn = document.querySelector(".for-open");
+var link = document.querySelector('.open-gallery');
+var gall = document.querySelector('.hidden-gallery');
+var btn = document.querySelector('.for-open');
 
-link.addEventListener("click", function(event) {
+link.addEventListener('click', function(event) {
 	event.preventDefault();
-	gall.classList.add("hidden-gallery-show");
-    btn.classList.add("hidden-btn");
+	gall.classList.add('hidden-gallery-show');
+    btn.classList.add('hidden-btn');
 });
