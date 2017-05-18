35ca55d0edc9ac4b963881855168ec5a290009ba
Katerina Komnatnaya <comina.ru@gmail.com>
2017-05-18 14:47:55 +0300

    поиск

--- js/open-gallery.js
+++ js/open-gallery.js
@@ -1,45 +1,9 @@
-var link = document.querySelectorAll('.open-gallery');
-var gall = document.querySelectorAll('.hidden-gallery');
-var btn = document.querySelectorAll('.for-open');
+var link = document.querySelector('.open-gallery');
+var gall = document.querySelector('.hidden-gallery');
+var btn = document.querySelector('.for-open');
 
-link[0].addEventListener('click', function(event) {
+link.addEventListener('click', function(event) {
 	event.preventDefault();
-	gall[0].classList.add('hidden-gallery-show');
-	btn[0].classList.add('hidden-btn');
-});
-
-link[1].addEventListener('click', function(event) {
-	event.preventDefault();
-	gall[1].classList.add('hidden-gallery-show');
-	btn[1].classList.add('hidden-btn');
-});
-
-link[2].addEventListener('click', function(event) {
-	event.preventDefault();
-	gall[2].classList.add('hidden-gallery-show');
-	btn[2].classList.add('hidden-btn');
-});
-
-link[3].addEventListener('click', function(event) {
-	event.preventDefault();
-	gall[3].classList.add('hidden-gallery-show');
-	btn[3].classList.add('hidden-btn');
-});
-
-link[4].addEventListener('click', function(event) {
-	event.preventDefault();
-	gall[4].classList.add('hidden-gallery-show');
-	btn[4].classList.add('hidden-btn');
-});
-
-link[5].addEventListener('click', function(event) {
-	event.preventDefault();
-	gall[5].classList.add('hidden-gallery-show');
-	btn[5].classList.add('hidden-btn');
-});
-
-link[6].addEventListener('click', function(event) {
-	event.preventDefault();
-	gall[6].classList.add('hidden-gallery-show');
-	btn[6].classList.add('hidden-btn');
+	gall.classList.add('hidden-gallery-show');
+	btn.classList.add('hidden-btn');
 });
---
+++ js/search.js
@@ -0,0 +1,35 @@
+var lastResFind = "";
+var copy_page = "";
+function TrimStr(s) {
+    s = s.replace(/^\s+/g, '');
+    return s.replace(/\s+$/g, '');
+}
+
+function FindOnPage(inputId) {
+    var obj = window.document.getElementById(inputId);
+    var textToFind;
+
+    if (obj) {
+        textToFind = TrimStr(obj.value);
+    } else {
+        alert("Введенная фраза не найдена");
+        return;
+    }
+    if (textToFind == "") {
+        alert("Вы ничего не ввели");
+        return;
+    }
+
+    if (document.body.innerHTML.indexOf(textToFind) == "-1")
+        alert("Ничего не найдено, проверьте правильность ввода!");
+
+    if (copy_page.length > 0)
+        document.body.innerHTML = copy_page;
+    else copy_page = document.body.innerHTML;
+
+
+    document.body.innerHTML = document.body.innerHTML.replace(eval("/name=" + lastResFind + "/gi"), " ");
+    document.body.innerHTML = document.body.innerHTML.replace(eval("/" + textToFind + "/gi"), "<a name=" + textToFind + " style='border-bottom:1px solid red'>" + textToFind + "</a>");
+    lastResFind = textToFind;
+    window.location = '#' + textToFind;
+}
